9d642b856bc2a8c9a6b882f9ab9bad3a79a05ecd
Ahmed Hariri <Ahmedhariri58@gmail.com>
2025-01-25 20:56:18 +0100

    Create structure project front-end && back-end

---
+++ front-end/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./styles/index.css";
+
+export const metadata: Metadata = {
+  title: "Etec || E-commerce",
+  description: "E-commerce web site",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
