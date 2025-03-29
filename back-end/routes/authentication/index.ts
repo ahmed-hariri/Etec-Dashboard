beab22ebcfd96712feb780088af33c7fca609cad
Ahmed Hariri <Ahmedhariri58@gmail.com>
2025-03-29 21:41:56 +0000

    Separate front-end and back-end into separate repositories

--- back-end/routes/authentication/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { signInController, signUpController } from "../../controllers/authentication";
-
-/*---> Define authentication routes <---*/
-export const authRoutes: Router = Router();
-
-authRoutes.post("/register", signUpController);
-authRoutes.post("/login", signInController);
--- back-end/routes/purchesed/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { authenticateToken, checkAdmin } from "../../middleware";
-import { getAllPurchesedController , removePurchesedController} from "../../controllers/purchesed";
-
-/*---> Define purchesed product routes <---*/
-export const purchesedRoutes: Router = Router();
-
-purchesedRoutes.get("/purcheseds", authenticateToken, checkAdmin, getAllPurchesedController)
-purchesedRoutes.delete("/purchesed/:id", authenticateToken, checkAdmin, removePurchesedController)
