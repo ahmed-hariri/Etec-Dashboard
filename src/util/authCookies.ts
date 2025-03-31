d89175f43364b625572758fcb58882e4bb8e26d7
Ahmed Hariri <Ahmedhariri58@gmail.com>
2025-03-31 10:52:32 +0000

    Fix CORS and cookies issues in deployed API

--- src/util/authCookies.ts
+++ src/util/authCookies.ts
@@ -1,5 +1,5 @@
 import Cookies from 'js-cookie';
 
 export const setAuthToken = (cookiesData: string) => {
-    Cookies.set("token", cookiesData, { expires: 7 });
+    Cookies.set("token", cookiesData, { expires: 7, secure: true, sameSite: "None" });
 }
